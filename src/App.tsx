f6cd63ab5ac2e9b3815d921344534949ca358e08
Josh Sanchez <joshuaesanch@gmail.com>
2023-08-28 15:22:10 -0500

    Add footer and header toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import './App.css';
-import { Container } from '@mui/material';
+import { AppBar, Button, Container, Divider, Toolbar, Typography } from '@mui/material';
 import Forecast from './components/Forecast';
 
 function App() {
   return (
     <div className="App">
-      <Container sx={{ maxWidth: '80%' }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography marginRight={"16px"}>Weather Monitor Service</Typography>
+          <Divider orientation='vertical' flexItem color='black' sx={{ width: "4px" }}/>
+          <Button sx={{ marginLeft: "10px" }} color="inherit">Home</Button>
+        </Toolbar>
+      </AppBar>
+
+      <Container sx={{ maxWidth: '80%', marginTop: "40px"}}>
         <Forecast />
       </Container>
+
+      <footer style={{marginTop: "20px"}}>
+        Icons by <a target="_blank" href="https://icons8.com">Icons8</a>
+      </footer>
     </div>
   );
 }
